cbdcd62d019eb35ff104143f85ca6a5240029a4c
axel kaban <y>
2018-04-01 11:18:26 -0400

    db set up with sequelize

--- index.js
+++ index.js
@@ -11,6 +11,80 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
+// CREATE DB
+var sequelize = new Sequelize('database', null, null, {
+      host: 'localhost',
+      dialect: 'sqlite',
+
+      storage: DB_PATH
+    });
+
+// DB TABLES
+
+const Genres = sequelize.define('genres', {
+  id: {
+    type: sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: sequelize.STRING,
+    allowNull: false
+  }
+})
+
+
+const Films = sequelize.define('films', {
+  id: {
+    type: sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: sequelize.STRING,
+    allowNull: false
+  },
+  release_date: {
+    type: sequelize.STRING,
+    allowNull: false
+  },
+  tagline: {
+    type: sequelize.STRING,
+    allowNull: false
+  },
+  revenue: {
+    type: sequelize.INTEGER,
+    allowNull: false
+  },
+  budget: {
+    type: sequelize.INTEGER,
+    allowNull: false
+  },
+  runtime: {
+    type: sequelize.INTEGER,
+    allowNull: false
+  },
+  original_language: {
+    type: sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+    type: sequelize.STRING,
+    allowNull: false
+  },
+  genre_id: {
+    type: sequelize.INTEGER,
+    allowNull: false,
+    // sequelize alows to connect to other models
+    references:{
+      model: Genres,
+      key: 'id'
+    }
+  }
+});
+
+
+
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
 
