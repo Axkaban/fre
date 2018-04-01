602c38f99eb3429e9ef77311e2e50ac98334fd76
axel kaban <y>
2018-04-01 13:09:59 -0400

    statement to validate limit and offset. getting object from id param

--- index.js
+++ index.js
@@ -23,57 +23,61 @@ var sequelize = new Sequelize('database', null, null, {
 
 const Genres = sequelize.define('genres', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false,
     primaryKey: true
   },
   name: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   }
+},
+{
+  timestamps: false // NEEDED to not get a createdAt and updatedAt fields on the query
 })
+
 
 
 const Films = sequelize.define('films', {
   id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false,
     primaryKey: true
   },
   title: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   },
   release_date: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   },
   tagline: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   },
   revenue: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false
   },
   budget: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false
   },
   runtime: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false
   },
   original_language: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   },
   status: {
-    type: sequelize.STRING,
+    type: Sequelize.STRING,
     allowNull: false
   },
   genre_id: {
-    type: sequelize.INTEGER,
+    type: Sequelize.INTEGER,
     allowNull: false,
     // sequelize alows to connect to other models
     references:{
@@ -81,16 +85,68 @@ const Films = sequelize.define('films', {
       key: 'id'
     }
   }
+}, 
+{
+  timestamps: false // NEEDED to not get a createdAt and updatedAt fields on the query
 });
 
 
 
+
 // ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+app.get('/films/:id([0-9]+)/recommendations', getFilmRecommendations); //getFilmRecommendations
+// if anything else rather than the route above it will handle a 404 response
+app.use('*', (req, res) => {
+  res.status(404).send({
+    Message: '404 This is not a valid route'
+  });
+});
+
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  // res.status(500).send('Not Implemented');
+  let recommendations = {
+    recommendations: []
+  };
+   // check if query values for metadata exist, else they're assigned default values
+   if (req.query.offset) {
+
+     if (req.query.limit) {
+
+       recommendations.meta = { 'limit': parseInt(req.query.limit) };
+       recommendations.meta = { 'offset': parseInt(req.query.offset) };
+
+     } else {
+
+       recommendations.meta = { 'limit': 10 };
+       recommendations.meta = { 'offset': parseInt(req.query.offset) };
+
+       
+     }
+  } else if (req.query.limit) {
+
+     recommendations.meta.limit = { 'limit': parseInt(req.query.limit) };
+     recommendations.meta.offset = { 'offset':1 };
+     
+  } else {
+
+     recommendations.meta.limit = { 'limit': 10 };
+     recommendations.meta.offset = { 'offset':1 };
+
+  }
+// 
+  Films.findById(req.params.id)
+  .then((filmData)=>{
+    if(!filmData){
+      res.status(404).send({
+        Message: 'There is no film with that id'
+      });
+    }
+    console.log(filmData.dataValues);
+    return filmData.dataValues;
+    
+  });
 }
 
 module.exports = app;
